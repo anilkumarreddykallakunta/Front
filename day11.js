d5aa361b9bac7609d37f02abdf728d18c96df68b
anilkumarreddykallakunta <anilreddy.k2266@gmail.com>
2025-06-12 22:33:18 +0530

    Keywords to Declare a Variable

--- day11.js
+++ day11.js
@@ -1,1 +1,13 @@
-console.log("HEllO");
+console.log(userName);
+var userName = "Anil";
+var userName = "Kumar";
+console.log(userName);
+
+
+let surName = "Kallakunta";
+surName = "Kumar";
+console.log(surName);
+
+const fullName= "Anil Kumar";
+fullName="kumar";
+console.log(fullName);
